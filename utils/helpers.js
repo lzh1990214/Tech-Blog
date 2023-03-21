aa6ef3e19230ca9fae55b1d76d5f2407cf1cb95e
zhihao li <zhihaoli@zhihaos-MBP.fios-router.home>
2023-03-20 23:07:54 -0400

    added breakline helper

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,6 +1,19 @@
+const Handlebars = require("handlebars");
+
 module.exports = {
+    // Format date as MM/DD/YYYY
     format_date: (date) => {
-        // Format date as MM/DD/YYYY
         return date.toLocaleDateString();
     },
+    // Slice text to only show first 150 characters
+    post_brief: str => {
+        const first150 = str.slice(0, 300);
+        return first150;
+    },
+    // Preserve the breaklines in long paragraphs
+    breaklines: (text) => {
+        text = Handlebars.Utils.escapeExpression(text);
+        text = text.replace(/(\r\n|\n|\r)/gm, '<br>');
+        return new Handlebars.SafeString(text);
+    }
 };
